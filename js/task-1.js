7c532f2adb4eb580b816fccf93b9fa2473b870a2
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-04-22 12:50:30 +0300

    Sunday

---
+++ js/task-1.js
@@ -0,0 +1,18 @@
+let makeTransaction = function multiply(a, b) {
+  console.log(`You ordered ${a * b}`);
+};
+// quantity;
+// pricePerDroid;
+// You ordered < quantity > droids worth < totalPrice > credits!
+// console.log(makeTransaction(5, 3000));
+// "You ordered 5 droids worth 15000 credits!"
+function add(a, b) {
+  console.log(`Addition result equals ${a + b}`);
+}
+
+add(15, 27);
+add(10, 20);
+add(5, 10);
+
+let hello = 5;
+console.log(hello);
