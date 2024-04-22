c63927c8e56f60fcb5af532eea75608712d8d6ad
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-04-22 15:33:03 +0300

    Monday task1,2,3ready
    .prettierrs

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,3 @@
-function makeTransaction(a, b) {
-  return `Your order ${a} droids worth ${a * b} credits!`;
-}
-
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
-
 function makeTransaction(quantity, pricePerDroid) {
   return `Your order ${quantity} droids worth ${
     quantity * pricePerDroid
